d30981091471952601d15178e7af423ae6ee03f0
glovee <superhomk@mail.ru>
2025-05-27 22:22:33 +0300

    Игорь, коммит для тебя. Просто по анологии допиши ручки опираясь на код из репозитория в который я тебя добавил (Oladik_Proj)

---
+++ db/validation.py
@@ -0,0 +1,106 @@
+from pydantic import BaseModel, EmailStr, constr
+from typing import List, Optional
+
+
+# === PROGRAM ===
+
+class ProgramBase(BaseModel):
+    program_name: str
+
+
+class ProgramCreate(ProgramBase):
+    pass
+
+
+class ProgramRead(ProgramBase):
+    program_id: int
+
+    class Config:
+        orm_mode = True
+
+
+# === USER ===
+
+class UserBase(BaseModel):
+    user_name: str
+    user_phone: str
+    user_email: EmailStr
+    user_password: str
+    user_group_id: int
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserRead(UserBase):
+    user_id: int
+
+    class Config:
+        orm_mode = True
+
+
+# === GROUP ===
+
+class GroupBase(BaseModel):
+    group_name: str
+
+
+class GroupCreate(GroupBase):
+    pass
+
+
+class GroupRead(GroupBase):
+    group_id: int
+    group_id_user: Optional[List[UserRead]] = []
+    g2p: Optional[List["GroupToProgRead"]] = []
+
+    class Config:
+        orm_mode = True
+
+
+# === G2P (Group_to_Prog) ===
+
+class GroupToProgBase(BaseModel):
+    group_id: int
+    program_id: int
+
+
+class GroupToProgCreate(GroupToProgBase):
+    pass
+
+
+class GroupToProgRead(GroupToProgBase):
+    relation_id: int
+    program: Optional[ProgramRead]
+    group: Optional[GroupRead]
+
+    class Config:
+        orm_mode = True
+
+
+# === ACCESS ROLES ===
+
+class AccessBase(BaseModel):
+    role_name: str
+    read_and_download_lessons: bool
+    generating_report: bool
+    voting: bool
+    checking_marks: bool
+    give_marks: bool
+
+
+class AccessCreate(AccessBase):
+    pass
+
+
+class AccessRead(AccessBase):
+    role_id: int
+
+    class Config:
+        orm_mode = True
+
+
+# Для вложенных ссылок (forward references)
+GroupRead.update_forward_refs()
+GroupToProgRead.update_forward_refs()
--- routers/creds_routers.py
+++ routers/creds_routers.py
@@ -13,6 +13,7 @@ from exceptions import CustomHTTPError
 from db.engine import SessionLocal
 from db.models import User
 
+from db.validation import UserCreate
 
 
 log: logging.Logger = logging.getLogger('lables_logic')
@@ -27,5 +28,8 @@ def get_db():
         yield session
 
 @Creds_Router.post('/create_user/')
-def create_user(db = Depends(get_db)):
-    new_user = User(user_name = )
+def create_user(user = Depends(UserCreate()), db = Depends(get_db)):
+    new_user = user(
+        user_name = UserCreate.user_name,
+        
+        )
